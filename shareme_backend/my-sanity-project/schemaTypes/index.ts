11fa5929aad3ac2fa324e918767752c305196c7c
Sammycode332 <samueloroja123@gmail.com>
2026-04-29 12:21:04 +0100

    Initial commit

---
+++ shareme_backend/my-sanity-project/schemaTypes/index.ts
@@ -0,0 +1,11 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
+import user from './user'
+import pin from './pin'
+import comment from './comment'
+import postedBy from './postedBy'
+import save from './save'
+
+export const schemaTypes = [post, author, category, blockContent, user, pin, comment, postedBy, save]
