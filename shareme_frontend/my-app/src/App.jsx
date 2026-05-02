807dc3d3bc732a93d6c20336cce5f36192f53e49
Sammycode332 <samueloroja123@gmail.com>
2026-05-02 07:31:50 +0100

    New Changes

---
+++ shareme_frontend/my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route,useNavigate} from 'react-router-dom';
+import Login from './components/login'
+import Home from './container/home'
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element = {<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
