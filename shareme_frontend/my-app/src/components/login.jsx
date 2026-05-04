a76ffdabe14d7f9acc31334ca3b6a5210faa15d1
Sammycode332 <samueloroja123@gmail.com>
2026-05-04 22:07:11 +0100

    Updated few changes

--- shareme_frontend/my-app/src/components/login.jsx
+++ shareme_frontend/my-app/src/components/login.jsx
@@ -3,8 +3,11 @@ import { GoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
-//import logo from '../assets/logowhite.png';
+import logo from '../assets/logowhite.png';
 const Login = () => {
+  const responseGoogle = (response)=>{
+
+  }
   return (
     <div className = "flex justify-start items-center flex-col h-screen">
       <div className = "relative w-full h-full">
@@ -19,9 +22,27 @@ const Login = () => {
         />
         <div className = "absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
           <div className='p-5'>
-            <img src = "https://via.placeholder.com/130x130/FFFFFF/000000?text=ShareMe"  width = "130px" alt='logo'/>
+            <img src = {logo}  width = "130px" alt='logo'/>
           </div>
-        </div> 
+
+          <div classname = "shadow-2xl">
+            <GoogleLogin 
+              clientId=''
+              render = {(renderProps)=>(
+                <button
+                  type = "button"
+                  className = "bg-maincolor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onclick = {renderProps.onclick}
+                  disabled = {renderProps.disabled}  
+                >
+                  <FcGoogle classname = "mr-4" />Sign in with Google
+                </button>
+              )}/>
+              onSuccess = {responseGoogle}
+              onFailure = {responseGoogle}
+              cookiePolicy = "single_host_origin"
+          </div>
+        </div>
       </div>
     </div>
     
