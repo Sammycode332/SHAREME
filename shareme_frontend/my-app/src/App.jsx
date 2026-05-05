d64b276e61ff54bd0f9ae00db7e9f8d7e9e5dde9
Sammycode332 <samueloroja123@gmail.com>
2026-05-05 09:27:29 +0100

    working models

--- shareme_frontend/my-app/src/App.jsx
+++ shareme_frontend/my-app/src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { BrowserRouter,Routes,Route,useNavigate} from 'react-router-dom';
-import Login from './components/login'
-import Home from './container/home'
+import { Routes,Route,useNavigate } from 'react-router-dom';
+import Login from './components/login';
+import Home from './container/home';
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/*" element = {<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <routes>
+      <Route path = "/login" element = {<Login />} />
+      <Route path = "/" element = {<Home />} />
+
+    </routes>
   )
 }
 
