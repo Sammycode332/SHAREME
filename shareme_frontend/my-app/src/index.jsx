639afd1181ea8463363479a945cc58152fc1babc
Sammycode332 <samueloroja123@gmail.com>
2026-05-07 22:37:08 +0100

    new changes

--- shareme_frontend/my-app/src/index.jsx
+++ shareme_frontend/my-app/src/index.jsx
@@ -5,11 +5,12 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './App.jsx';
 
-const GOOGLE_CLIENT_ID = '407408718192.apps.googleusercontent.com'; 
+const GOOGLE_CLIENT_ID = '410643880064-r15iuuocde6lu1eqtqlb03ach0fshff7.apps.googleusercontent.com'; 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID} auto_select={false}>
+
             <App />
         </GoogleOAuthProvider>
     </React.StrictMode>
