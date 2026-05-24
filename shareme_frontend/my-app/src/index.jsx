ae1433f2c67de894fefe5c902e60bfb13b529688
Sammycode332 <samueloroja123@gmail.com>
2026-05-24 13:05:10 +0100

    fixed bugs in my index page

--- shareme_frontend/my-app/src/index.jsx
+++ shareme_frontend/my-app/src/index.jsx
@@ -6,7 +6,6 @@ import App from './App.jsx';
 
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-console.log("GOOGLE_CLIENT_ID:", GOOGLE_CLIENT_ID)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
