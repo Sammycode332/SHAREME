5c09de512cc5478d97cb044e4504fe47ba6fa533
Sammycode332 <samueloroja123@gmail.com>
2026-05-24 13:07:58 +0100

    created a client.js to import from my env

---
+++ shareme_frontend/my-app/src/client.js
@@ -0,0 +1,13 @@
+import { createClient } from '@sanity/client'
+import { createImageUrlBuilder } from '@sanity/image-url'
+
+export const client = createClient({
+    projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+    dataset: 'production',
+    apiVersion: '2021-11-16',
+    useCdn: true,
+    token: import.meta.env.VITE_SANITY_TOKEN
+});
+
+const builder = createImageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
