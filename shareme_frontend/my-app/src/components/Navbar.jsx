f579d168888fc9cf0ebc291109aca7f420a454cc
Sammycode332 <samueloroja123@gmail.com>
2026-05-27 16:17:51 +0100

    Refactor Navbar component props destructuring and improve user image handling

--- shareme_frontend/my-app/src/components/Navbar.jsx
+++ shareme_frontend/my-app/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, useNavigate} from 'react-router-dom';
 
 import { IoMdAdd,IoMdSearch } from 'react-icons/io';
-const Navbar = (searchTerm,setSearchTerm,user) => {
+const Navbar = ({searchTerm,setSearchTerm,user}) => {
     const navigate = useNavigate();
 
       if(!user) return null;
@@ -12,7 +12,7 @@ const Navbar = (searchTerm,setSearchTerm,user) => {
     <div className = "flex gap-2 md:gap-5 w-full mt-5 pb-7">
         <div className = "flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
             <IoMdSearch fontSize = {21} className = "ml-1" />
-            <input X
+            <input 
                 type = "text"
                 placeholder = "Search"
                 value = {searchTerm}
@@ -24,7 +24,7 @@ const Navbar = (searchTerm,setSearchTerm,user) => {
         </div>
         <div className = "flex gap-3">
             <Link to = {`user-profile/${user?._id}`} className = "hidden md:block">
-                <img src = {user.image} alt = "user-image" className = "w-14 h-12 rounded-lg" />
+                <img src = {user?.picture || user?.image} alt = "user-image" className = "w-14 h-12 rounded-lg" />
             </Link>
             <Link to = "create-pin" className = "bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center">
                 <IoMdAdd />
