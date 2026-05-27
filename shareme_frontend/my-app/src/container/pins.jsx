e2687b3b4ada45e0b57d008281dde8311280ade8
Sammycode332 <samueloroja123@gmail.com>
2026-05-27 16:17:31 +0100

    Add user prop to Navbar component in Pins container

--- shareme_frontend/my-app/src/container/pins.jsx
+++ shareme_frontend/my-app/src/container/pins.jsx
@@ -8,9 +8,9 @@ const Pins = ( { user } ) => {
   return (
     <div className = "px-2 md:px-5">
       <div className = "bg-gray-50">
-         <Navbar searchTerm = {searchTerm} setSearchTerm = {setSearchTerm}/>
+         <Navbar searchTerm = {searchTerm} setSearchTerm = {setSearchTerm} user= {user}/>
       </div>
-       <div className = "h-full">
+       <div className = "h-full ">
         <Routes>
           <Route path = "/" element = {<Feed />} />
           <Route path = "/category/:categoryId" element = {<Feed />} />
