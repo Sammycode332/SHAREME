cc96533c737958e12a6012f17b2c3e085b2c20fc
Sammycode332 <samueloroja123@gmail.com>
2026-05-27 08:59:23 +0100

    Initial Commit

---
+++ shareme_frontend/my-app/src/components/Feed.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect }from 'react';
+import { useParams } from 'react-router-dom';
+
+const Feed = () => {
+  return (
+    <div>Feed</div>
+  )
+}
+
+export default Feed
