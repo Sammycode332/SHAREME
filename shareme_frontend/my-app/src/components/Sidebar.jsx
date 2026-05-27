2e200cf89591c386c2c1a0aa6f08d81a1f7d3484
Sammycode332 <samueloroja123@gmail.com>
2026-05-27 16:18:06 +0100

    Update Sidebar component to handle user profile links and images more robustly

--- shareme_frontend/my-app/src/components/Sidebar.jsx
+++ shareme_frontend/my-app/src/components/Sidebar.jsx
@@ -54,12 +54,12 @@ const Sidebar = ({ user, closeToggle }) => {
       </div>
       {user && (
         <Link
-          to = {`user-profile/${user._id}`} 
+          to = {`user-profile/${user?._id || user?.sub}`}
           className ="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
           onClick = {handleCloseSidebar}
           
           >
-            <img src = {user.image} className = "w-10 h-10 rounded full" alt = "user-profile"/>
+            <img src = {user?.picture || user?.image} className = "w-10 h-10 rounded-full" alt = "user-profile"/>
             <p>{user.userName}</p>
         </Link>
       )}
