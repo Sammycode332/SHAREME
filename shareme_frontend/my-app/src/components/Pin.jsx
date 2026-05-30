563c5832d04cff7069ae2b22d73096107e7e9f0b
Sammycode332 <samueloroja123@gmail.com>
2026-05-31 00:09:29 +0100

    Enhance Pin component to include save functionality and user info retrieval

--- shareme_frontend/my-app/src/components/Pin.jsx
+++ shareme_frontend/my-app/src/components/Pin.jsx
@@ -4,13 +4,18 @@ import { v4 as uuidv4 } from 'uuid';
 import { MdDownloadForOffline } from 'react-icons/md';
 import { AiTwotoneDelete } from 'react-icons/ai';
 import { BsFillArrowUpRightCircleFill} from 'react-icons/bs';
-
+import { fetchUser } from '../utils/fetchUser.js';
 
 import { client,urlFor } from '../client';
-const Pin = ({ pin: {postedBy,image, _id,destination}})=> {
+const Pin = ({ pin: {postedBy,image, _id,destination, save}})=> {
     const [postHovered, setPostHovered] = useState(false);
     const[savingPost, setSavingPost] = useState(false);
     const navigate = useNavigate();
+
+    const userInfo = fetchUser();
+    const alreadySaved = !!(save?.filter((item)=> item.postedBy._id === user.googleId))?.length;
+
+
   return (
     <div className = "m-2">
         <div
@@ -23,8 +28,29 @@ const Pin = ({ pin: {postedBy,image, _id,destination}})=> {
            {postHovered && (
             <div 
               className = "absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50" 
+              style ={{height:'100%'}}
             >
-
+              <div className = "flex items-center justify-between">
+                <div className = "flex-gap-2">
+                  <a
+                  href = {`${image?.asset?.url}?dl=`} 
+                  download
+                  onClick = {(e) => e.stopPropagation()}
+                  className = "bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+                  >
+                    <MdDownloadForOffline/>
+                  </a>
+                </div>
+                {alreadySaved ? (
+                  <button type = "button" className = "bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outlined-none" >
+                    Saved
+                  </button>
+                ) : (
+                  <button type= "button" className = "bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outlined-none">
+                    Save
+                  </button>
+                )}
+              </div>
             </div>
            )}
         </div>
