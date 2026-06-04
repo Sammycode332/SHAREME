62ce103dbfc27c19a48e97b84b3d58d9db9600df
Sammycode332 <samueloroja123@gmail.com>
2026-06-04 02:46:02 +0100

    fix: handle case when no pins are available in the feed

--- shareme_frontend/my-app/src/components/Feed.jsx
+++ shareme_frontend/my-app/src/components/Feed.jsx
@@ -27,6 +27,7 @@ const Feed = () => {
   },[categoryId])
 
   if(loading) return <Spinner message = "We are adding new ideas to your feed!" />
+  if(!pins?.length) return <h2 className = "text-center font-bold text-2xl mt-10 !text-black">No pins available</h2>
   return (
     <div>
       {pins && <MasonryLayout pins= {pins}/>}
