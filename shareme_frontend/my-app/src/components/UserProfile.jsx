5dbe1f1fa2f380fffaa147839f3ce04a9fa8e4f0
Sammycode332 <samueloroja123@gmail.com>
2026-06-04 02:46:52 +0100

    feat: enhance UserProfile component with user data fetching, logout functionality, and dynamic pin display

--- shareme_frontend/my-app/src/components/UserProfile.jsx
+++ shareme_frontend/my-app/src/components/UserProfile.jsx
@@ -1,8 +1,131 @@
-import React from 'react'
+import React, {useState, useEffect }from 'react'
+import { AiFillCloseCircle } from 'react-icons/ai';
+import {useParams, useNavigate} from 'react-router-dom';
+import { googleLogout } from '@react-oauth/google';
 
+import { userCreatedPinsQuery, userQuery, userSavedPinsQuery } from '../utils/data';
+import {client} from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+const randomImage = "https://picsum.photos/1600/900";
+
+const activeBtnStyles = 'bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none';
+const notActiveBtnStyles = 'bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none';
 const UserProfile = () => {
+  const [user, setUser] = useState(null);
+  const [pins, setPins] = useState(null);
+  const [text, setText] = useState('Created'); //created or saved
+  const [activeBtn, setActiveBtn] = useState('created');
+ 
+  const navigate = useNavigate();
+  const {userId} = useParams();
+  const handleLogout = () => {
+  googleLogout();
+  localStorage.removeItem('user');
+
+  navigate('/login');
+};
+
+  
+   useEffect(() => {
+    const query = userQuery(userId);
+    client.fetch(query)
+    .then((data) => {
+      setUser(data[0]);
+    })
+  }, [userId]);
+
+  useEffect(() => {
+    if(text === 'Created'){
+      const createdPinsQuery = userCreatedPinsQuery(userId);
+
+      client.fetch(createdPinsQuery)
+      .then((data) => {
+        setPins(data);
+      })
+    }else{
+      const savedPinsQuery = userSavedPinsQuery(userId);
+       client.fetch(savedPinsQuery)
+      .then((data) => {
+        setPins(data);
+      });
+  }
+}, [text, userId]);
+    
+   if(!user){
+    return <Spinner message="Loading profile..."/>
+  }
   return (
-    <div>UserProfile</div>
+    <div className='relative pb-2 h-full justify-center items-center text-black'>
+  <div className='flex flex-col pb-5'>
+    <div className='relative flex flex-col mb-7'>
+      <div className='flex flex-col justify-center items-center'>
+
+        <img
+          src={randomImage}
+          className='w-full h-370 2xl:h-510 shadow-lg object-cover'
+          alt="banner-pic"
+        />
+
+        <img
+          className='rounded-full w-20 h-20 -mt-10 shadow-xl object-cover'
+          src={user.image}
+          alt="user-pic"
+        />
+
+        <h1 className='font-bold text-3xl text-center mt-3 !text-black'>
+          {user.userName}
+        </h1>
+
+        <div className='absolute top-0 z-10 right-0 p-2'>
+          {userId === user?._id && (
+            <button
+              type="button"
+              onClick={handleLogout}
+              className="bg-red-500 text-white px-4 py-2 rounded-full mt-2"
+            >
+              Logout
+            </button>
+          )}
+        </div>
+      </div>
+      <div className='text-center mb-7'>
+        <button
+          type="button"
+          onClick={(e) => {
+            setActiveBtn('created');
+            setText(e.target.textContent);
+          }}
+          className={`${activeBtn === 'created' ? activeBtnStyles : notActiveBtnStyles}`}
+        >
+          Created
+        </button>
+        <button
+          type="button"
+          onClick={(e) => {
+            setActiveBtn('saved');
+            setText(e.target.textContent);
+          }}
+          className={`${activeBtn === 'saved' ? activeBtnStyles : notActiveBtnStyles}`}
+        >
+          Saved
+        </button>
+      </div>
+      {pins?.length ? (
+        <div className='px-2'>
+          <MasonryLayout pins={pins} />
+        </div>
+      ) : (
+        <div>
+          <div className='flex justify-center font-bold items-center w-full text-1xl mt-2'>
+             No Pins Found!
+            </div>
+          </div>
+      )}
+    </div>
+  </div>
+</div>
   )
 }
 
