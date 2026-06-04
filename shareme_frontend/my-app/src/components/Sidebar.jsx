3f9e8a47e62d13b2bbf8e008830090aeae621c57
Sammycode332 <samueloroja123@gmail.com>
2026-06-04 02:46:42 +0100

    feat: refactor Sidebar component to import categories and enhance UI with category images

--- shareme_frontend/my-app/src/components/Sidebar.jsx
+++ shareme_frontend/my-app/src/components/Sidebar.jsx
@@ -7,22 +7,15 @@ import logo from '../assets/logo.png';
 const isNotActiveStyle = "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize"
 const isActiveStyle = "flex items-center px-5 gap-3 font-extra-bold border-r-2 border-black transition-all duration-200 ease-in-out capitalize"
 
-const categories = [
-  {name : 'Animals'},
-  {name: 'Wallpapers'},
-  {name: 'Photography'},
-  {name: 'Gaming'},
-  {name: 'Coding'},
-  {name: 'Other'},
+import { categories } from '../utils/data';
 
-]
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
     if(closeToggle) closeToggle(false);
 
   }
   return (
-    <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll w-full min-w-210 hide-scrollbar">
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-auto w-full min-w-210 hide-scrollbar">
       <div className = "flex flex-col">
         <Link 
           to = "/"
@@ -47,6 +40,7 @@ const Sidebar = ({ user, closeToggle }) => {
                  onClick = {handleCloseSidebar}
                  key = {category.name}
                  >
+                  <img src = {category.image} className = "w-8 h-8 rounded-full shadow-sm" alt = "category" />
                 {category.name}
                 </NavLink>
             ))}
@@ -57,7 +51,7 @@ const Sidebar = ({ user, closeToggle }) => {
           to = {`user-profile/${user?._id || user?.sub}`}
           className ="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
           onClick = {handleCloseSidebar}
-          
+           onError={() => console.log("FAILED:", category.image)}
           >
             <img src = {user?.picture || user?.image} className = "w-10 h-10 rounded-full" alt = "user-profile" referrerPolicy="no-referrer"/>
             <p>{user.userName}</p>
