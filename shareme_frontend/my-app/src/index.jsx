a36a715aa6e9c8f19d6a633be34af612c2d60795
Sammycode332 <samueloroja123@gmail.com>
2026-06-06 11:52:05 +0100

    feat: refactor index.jsx to wrap App component with BrowserRouter for routing support

--- shareme_frontend/my-app/src/index.jsx
+++ shareme_frontend/my-app/src/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './App.jsx';
-
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID} auto_select={false}>
-
-            <App />
-        </GoogleOAuthProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID} auto_select={false}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
 );
